74a047201689a4a54b45e999d6d886d4bfe3a168
swapnildhiman19 <swapnildhiman1999@gmail.com>
2025-09-22 19:17:01 +0530

    Stack and Queue, Longest Common Prefix, Longest Palindrome Substring

--- StackAndQueuesImplementation.playground/Contents.swift
+++ StackAndQueuesImplementation.playground/Contents.swift
@@ -1,35 +1,35 @@
 import Foundation
 
-class ArrayStack<T> {
-
-    private var stack: [T]
-
-    init() {
-        stack = []
-    }
-
-    func push(_ x: T) {
-        stack.append(x)
-    }
-
-    func pop() -> T? {
-        guard !isEmpty() else {
-            return nil
-        }
-        return stack.removeLast()
-    }
-
-    func top() -> T? {
-        guard !isEmpty() else {
-            return nil
-        }
-        return stack.last
-    }
-
-    func isEmpty() -> Bool {
-        stack.isEmpty
-    }
-}
+//class ArrayStack<T> {
+//
+//    private var stack: [T]
+//
+//    init() {
+//        stack = []
+//    }
+//
+//    func push(_ x: T) {
+//        stack.append(x)
+//    }
+//
+//    func pop() -> T? {
+//        guard !isEmpty() else {
+//            return nil
+//        }
+//        return stack.removeLast()
+//    }
+//
+//    func top() -> T? {
+//        guard !isEmpty() else {
+//            return nil
+//        }
+//        return stack.last
+//    }
+//
+//    func isEmpty() -> Bool {
+//        stack.isEmpty
+//    }
+//}
 
 //class ArrayQueue<T> {
 //
@@ -72,41 +72,99 @@ class ArrayStack<T> {
  
  Using head counter and to return for majority of times, if storage becomes too long then we delete that prefix of head elements and reset the array
  */
-class ArrayQueue<T> {
-    private var queue: [T] = []
-    private var head: Int = 0
+//class ArrayQueue<T> {
+//    private var queue: [T] = []
+//    private var head: Int = 0
+//
+//    // Enqueue (push)
+//    func push(_ x: T) {
+//        queue.append(x)
+//    }
+//
+//    // Dequeue (pop)
+//    func pop() -> T? {
+//        guard head < queue.count else { return nil }
+//        let element = queue[head]
+//        head += 1
+//
+//        // Periodically clean up storage if head becomes large
+//        if head > 50 && head * 2 > queue.count {
+//            queue.removeFirst(head) //Remove first head elements from the array
+//            head = 0
+//        }
+//
+//        return element
+//    }
+//
+//    // Peek (front/top)
+//    func top() -> T? {
+//        guard head < queue.count else { return nil }
+//        return queue[head]
+//    }
+//
+//    // Is Empty
+//    func isEmpty() -> Bool {
+//        head >= queue.count
+//    }
+//}
 
-    // Enqueue (push)
-    func push(_ x: T) {
-        queue.append(x)
+
+//
+//// Stack implementation using Queue
+//class MyStack {
+//    var queue = [Int]()
+//
+//    init() {}
+//    
+//    // O(n) : operation
+//    func push(_ x: Int) {
+//        queue.append(x)
+//        // Rotate all previous elements to the back
+//        for _ in 0..<queue.count-1 {
+//            queue.append(queue.removeFirst())
+//        }
+//    }
+//    
+//    // O(1) : operation
+//    func pop() -> Int {
+//        return queue.isEmpty ? -1 : queue.removeFirst()
+//    }
+//    
+//    // O(1) : operation
+//    func top() -> Int {
+//        return queue.isEmpty ? -1 : queue.first!
+//    }
+//
+//    func empty() -> Bool {
+//        return queue.isEmpty
+//    }
+//}
+
+//Queue using stack
+class MyQueue {
+    
+    var stack = [Int]()
+
+    init() {
+        
     }
-
-    // Dequeue (pop)
-    func pop() -> T? {
-        guard head < queue.count else { return nil }
-        let element = queue[head]
-        head += 1
-
-        // Periodically clean up storage if head becomes large
-        if head > 50 && head * 2 > queue.count {
-            queue.removeFirst(head) //Remove first head elements from the array
-            head = 0
+    
+    func push(_ x: Int) {
+        stack.append(x)
+        for index in 0..<stack.count - 1 {
+            stack.append(stack.remove(at: index))
         }
-
-        return element
     }
-
-    // Peek (front/top)
-    func top() -> T? {
-        guard head < queue.count else { return nil }
-        return queue[head]
+    
+    func pop() -> Int {
+        return stack.isEmpty ? -1 : stack.removeLast()
     }
-
-    // Is Empty
-    func isEmpty() -> Bool {
-        head >= queue.count
+    
+    func peek() -> Int {
+        return stack.isEmpty ? -1 : stack.last!
+    }
+    
+    func empty() -> Bool {
+        return stack.isEmpty
     }
 }
-
-
-
